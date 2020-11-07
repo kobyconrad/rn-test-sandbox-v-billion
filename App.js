557d9c048d86e9fe91db149f15d9e8ad1cf04114
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-11-07 14:19:34 -0800

    added react navigation from docs

--- App.js
+++ App.js
@@ -1,48 +1,49 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import "react-native-gesture-handler";
-import { StyleSheet, Text, View, Button } from "react-native";
+// In App.js in a new project
+
+import * as React from "react";
+import { View, Text, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate("Details")}
+      />
+    </View>
+  );
+}
+
 const Stack = createStackNavigator();
 
-export default function App() {
+function App() {
   return (
     <NavigationContainer>
-      {/* <View style={styles.container}>
-        <Text>Nice</Text>
-        <StatusBar style="auto" />
-      </View> */}
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen
           name="Home"
           component={HomeScreen}
-          options={{ title: "Welcome" }}
+          options={{ title: "Overview" }}
         />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
-
-const HomeScreen = ({ navigation }) => {
+function DetailsScreen({ navigation }) {
   return (
-    <Button
-      title="Go to Jane's profile"
-      onPress={() => navigation.navigate("Profile", { name: "Jane" })}
-    />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.navigate("Details")}
+      />
+    </View>
   );
-};
-const ProfileScreen = () => {
-  return <Text>This is Jane's profile</Text>;
-};
+}
+
+export default App;
