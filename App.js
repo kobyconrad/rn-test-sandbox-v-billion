59944711f6268cbd12a0cf2869822afd4a9a4716
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-11-11 10:18:15 -0800

    implemented local storage

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import * as React from "react";
 import { View, Text, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Stack = createStackNavigator();
 
@@ -26,10 +27,44 @@ function App() {
   );
 }
 
+const storeData = async (value) => {
+  try {
+    const jsonValue = JSON.stringify(value);
+    await AsyncStorage.setItem("randomNumb", jsonValue);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const getData = async () => {
+  try {
+    const value = await AsyncStorage.getItem("randomNumb");
+    if (value !== null) {
+      console.log("success: ", value);
+    }
+  } catch (e) {}
+};
+
 function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Quit Counter</Text>
+      <Button
+        title="Random Number"
+        onPress={() => {
+          let ranNumb = Math.random();
+          console.log(ranNumb);
+
+          storeData(ranNumb);
+        }}
+      />
+      <Button
+        title="Read Data"
+        onPress={() => {
+          console.log("read data");
+          getData();
+        }}
+      />
       <Button
         title="Reset Counter"
         onPress={() => navigation.navigate("Reset")}
